99ef5d2534f7ad21844fb15f046674c6ee102766
Ronald Arias <ariasron@hotmail.com>
2022-10-25 16:34:36 -0500

    uses getServerSideProps to prefetch the data

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 const PageLayout = ({children, title='News App'}) => {
   return (
@@ -8,12 +9,21 @@ const PageLayout = ({children, title='News App'}) => {
         <meta name="description" content="newsapp - the best app to read news" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>🗞 newsapi</header>
+      <header>
+        <div>
+          <Link href="/">🗞 newsapi</Link>
+        </div>
+        <div>
+          <Link href="/about">About</Link>
+        </div>
+      </header>
       <main>
         {children}
       </main>
       <style jsx>{`
       header {
+        display: flex;
+        justify-content: space-between;
         padding: 20px;
       }
     `}</style>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
 
-import Link from 'next/link'
+import Image from 'next/image'
 import PageLayout from '../components/PageLayout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({articles}) {
+  console.log(articles);
   return (
     <PageLayout>
       <div className={styles.container}>
-        <h1>Next.js from scratch</h1>
-        <Link href='/about'>About</Link>
+        {articles.length === 0 && <p>There are no articles</p>}
+        {articles.length > 0 && articles.map((article, index) => (
+          <article key={index}>
+            <img 
+              src={article.urlToImage} 
+              alt={`Image for the article ${article.title}`}
+              width={300}
+              height={169}
+              />
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+          </article>
+        ))}
       </div>
     </PageLayout>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b5dbd67693af4df080df4680eb8b164b')
+  const {articles} = await response.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
