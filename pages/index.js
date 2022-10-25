a83c2b648baf134a6347e25b6c238ba3b3b6db8e
Ronald Arias <ariasron@hotmail.com>
2022-10-25 16:52:12 -0500

    uses getStaticProps to call the api fetch only once

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,8 @@ export default function Home({articles}) {
   )
 }
 
-export async function getServerSideProps() {
+// N requests -> will execute only 1 or when refreshing the page
+export async function getStaticProps() {
   const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b5dbd67693af4df080df4680eb8b164b')
   const {articles} = await response.json()
 
@@ -36,3 +37,17 @@ export async function getServerSideProps() {
     }
   }
 }
+
+// N requests -> will execute N times
+// used for data live
+// used for a lot of dynamic data
+// export async function getServerSideProps() {
+//   const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b5dbd67693af4df080df4680eb8b164b')
+//   const {articles} = await response.json()
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
